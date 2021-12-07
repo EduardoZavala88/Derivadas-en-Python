e2ece35c364dcd622d935e1b3de60684aa4ac499
EduardoZavala88 <95717456+EduardoZavala88@users.noreply.github.com>
2021-12-07 14:24:47 -0600

    Add files via upload

---
+++ derivadapy.py
@@ -0,0 +1,23 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.0.31912.275
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{888888A0-9F3D-457C-B088-3A5042F75D52}") = "derivadapy", "derivadapy\derivadapy.pyproj", "{EF2750EC-6F90-4A9F-8338-741BCDFAA14E}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{EF2750EC-6F90-4A9F-8338-741BCDFAA14E}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{EF2750EC-6F90-4A9F-8338-741BCDFAA14E}.Release|Any CPU.ActiveCfg = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {67E227F0-7052-4CEE-9DF9-3CE70A92A7EC}
+	EndGlobalSection
+EndGlobal
